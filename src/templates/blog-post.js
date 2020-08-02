2cd791f8d10ee15d16fe83a7759ad28e35324c6c
Husni Munaya <husnimun@gmail.com>
2020-08-02 15:30:25 +0700

    Add utterances comment

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
@@ -10,6 +10,20 @@ const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
   const siteTitle = data.site.siteMetadata.title
 
+  useEffect(() => {
+    const script = document.createElement("script")
+    const comments = document.getElementById("comments")
+    script.src = "https://utteranc.es/client.js"
+    script.async = true
+    script.setAttribute("repo", "husnimun/husni.dev.comments")
+    script.setAttribute("issue-term", "pathname")
+    script.setAttribute("repo", "husnimun/husni.dev.comments")
+    script.setAttribute("label", "comment")
+    script.setAttribute("theme", "github-light")
+    script.setAttribute("crossorigin", "anonymous")
+    comments.appendChild(script)
+  }, [])
+
   return (
     <Layout location={location} title={siteTitle}>
       <SEO
@@ -19,13 +33,13 @@ const BlogPostTemplate = ({ data, location }) => {
       <article>
         <header className={styles.Header}>
           <h1 className={styles.HeaderTitle}>{post.frontmatter.title}</h1>
-          <p className={styles.HeaderDate}>
-            {post.frontmatter.date}
-          </p>
+          <p className={styles.HeaderDate}>{post.frontmatter.date}</p>
         </header>
         <section dangerouslySetInnerHTML={{ __html: post.html }} />
         <footer></footer>
       </article>
+      <hr className={styles.Separator}></hr>
+      <div id="comments" className={styles.Comments}></div>
     </Layout>
   )
 }
